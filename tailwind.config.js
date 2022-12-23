ad5a10a20a1825d76954ed12bd3db848135e063f
Freddy Cunningham <freddy@fcunningham.co.uk>
2022-12-23 22:53:42 +0000

    font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
     extend: {}
-  },
-  plugins: []
+  }
+  // plugins: [
+  //   plugin(function ({ addBase }) {
+  //     addBase({
+  //       html: { fontSize: '16px' }
+  //     });
+  //   })
+  // ]
 };
